5ba7bd12728b2675bc8ceed4f5ca44dc5d4af373
Dario <dario.righelli@gmail.com>
2019-07-15 18:18:40 +0200

    check passed

--- inst/examples/first_example.R
+++ inst/examples/first_example.R
@@ -35,10 +35,11 @@ rd$mkdCodeChunkComplete(message="a <- 1\nb <- 2\nc <- a+b\n print(c)")
 ## otherwhise I can make a direct call with all the code chunk and the comment in one call
 optList <- makeOptionsList(includeFlag=TRUE, cacheFlag=TRUE)
 
-rd$mkdCodeChunkCommented(commentMsg="This is the comment of the following code chunk",
-                    message="a <- 1\nb <- 2\n(c <- a+b)\n",
-                    optionsList=optList,
-                    source.files.list=NULL)
+rd$mkdCodeChunkCommented(
+                commentMsg="This is the comment of the following code chunk",
+                codeMsg="a <- 1\nb <- 2\n(c <- a+b)\n",
+                optionsList=optList,
+                sourceFilesList=NULL)
 
 ## finally I can directly compile my report
 rd$compile()
